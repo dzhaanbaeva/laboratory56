705fb4ac9df18a85e116a610053e99322ea610fc
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-03-30 18:15:41 +0600

    Initial commit

---
+++ src/test/java/com/attractor/laboratory56/Laboratory56ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attractor.laboratory56;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratory56ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
